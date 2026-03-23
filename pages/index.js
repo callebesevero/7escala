7f5236da5743f967f029928038f718d0b4a22374
Callebe Severo <callebesev@gmail.com>
2026-03-23 11:58:20 -0300

    get serviceDays

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import * as dt from "date-fns";
 import { ptBR } from "date-fns/locale";
 
-const init = new Date(2026, 2, 1)
-const end = dt.endOfMonth(init)
+const init = new Date(2026, 2, 1);
+const end = dt.endOfMonth(init);
 
-const allOfDays = dt.eachDayOfInterval({start: init, end: end})
+const allOfDays = dt.eachDayOfInterval({start: init, end: end}); // get all days of month
 
-const fieldDate = document.querySelector("#date")
-fieldDate.innerHTML = allOfDays
+const serviceDays = allOfDays.filter(day => {
+    const weekDay = dt.getDay(day);
+    return weekDay === 0 || weekDay === 3 || weekDay === 6;
+});
+
+serviceDays.forEach(date => {
+    dt.format(date, "eeee, dd 'de' MMMM", { locale: ptBR });
+});
+
+const fieldDate = document.querySelector("#date");
+fieldDate.innerHTML = serviceDays;
