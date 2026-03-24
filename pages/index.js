85e979a455bba792d1fc864de29f8ebc16b90f3f
Callebe Severo <callebesev@gmail.com>
2026-03-24 12:43:46 -0300

    index fields id / import parseToJSON

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { serviceDays as dates } from "./getDates";
+import { parseToJSON } from "./json";
 
 const serviceDays = dates.map(date => {
-    return `<div>${date}<input type="text" id="${date}"></div>`;
+    return `<div><label for="${date}">${date}</label><input type="text" id="${date}"></div>`;
 });
 
 const fieldDate = document.querySelector("#date");
 fieldDate.innerHTML = serviceDays;
 fieldDate.innerHTML += `<div><input id="confirm" type="button" value="Confirmar escala"></div>`;
+parseToJSON()
