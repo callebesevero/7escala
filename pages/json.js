87924b52054276f08649ccef836a0470a7fce098
Callebe Severo <callebesev@gmail.com>
2026-03-24 14:31:23 -0300

    json send

--- pages/json.js
+++ pages/json.js
@@ -3,32 +3,20 @@ export function parseToJSON() {
     const dates = document.querySelector("#date");
     
     confirmButton.addEventListener("click", (event) => {
-        // pegar informações
-        // passar pra array
-        // converter arrays em json
         const label = Array.from(document.querySelectorAll("label")).map(lb => { // Array.from() because document.querySelectorAll("label") returns a NodeList
             return lb.textContent;
         });
         const input = Array.from(document.querySelectorAll("input")).map(inp => {
             return inp.value;
         });
-    
-        console.log(`${label[0]}\n${input}`);
-        console.log(typeof label)
-        console.log(typeof input)
+
+        const escalaArray = label.map((lb, i) => {
+            return {
+                "date": lb,
+                "people": input[i]
+            };
+        });
+
+        const escalaJSON = JSON.stringify(escalaArray);
     });
 };
-/*
-"escala": {
-    "domingo, 01 de março": [
-        "pessoa",
-        "pessoa2",
-        "pessoa3"
-    ],
-    "quarta, 04 de março": [
-        "pessoa",
-        "pessoa2",
-        "pessoa3"
-    ]
-}
-*/
