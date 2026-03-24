97cae7d0dbb71455ed5a6a6dd020e02cfe13e462
Callebe Severo <callebesev@gmail.com>
2026-03-24 09:44:48 -0300

    format

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ const end = dt.endOfMonth(init);
 
 const allOfDays = dt.eachDayOfInterval({start: init, end: end}); // get all days of month
 
-const serviceDays = allOfDays.filter(day => {
+const serviceDaysUnformated = allOfDays.filter(day => {
     const weekDay = dt.getDay(day);
     return weekDay === 0 || weekDay === 3 || weekDay === 6;
 });
 
-serviceDays.forEach(date => {
-    dt.format(date, "eeee, dd 'de' MMMM", { locale: ptBR });
+const serviceDays = serviceDaysUnformated.map(date => {
+    return dt.format(date, "eeee, dd 'de' MMMM", { locale: ptBR });
 });
 
 const fieldDate = document.querySelector("#date");
