20e8c2dd76a1ed79280eb33172086ef0f764e6f2
Callebe Severo <callebesev@gmail.com>
2026-03-24 10:08:37 -0300

    old index -> getDates / and index put fields on html

---
+++ pages/getDates.js
@@ -0,0 +1,18 @@
+import * as dt from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+const init = new Date(2026, 2, 1);
+const end = dt.endOfMonth(init);
+
+const allOfDays = dt.eachDayOfInterval({start: init, end: end}); // get all days of month
+
+const serviceDaysUnformated = allOfDays.filter(day => {
+    const weekDay = dt.getDay(day);
+    return weekDay === 0 || weekDay === 3 || weekDay === 6;
+});
+
+const serviceDays = serviceDaysUnformated.map(date => {
+    return dt.format(date, "eeee, dd 'de' MMMM", { locale: ptBR });
+});
+
+export { serviceDays }
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,9 @@
-import * as dt from "date-fns";
-import { ptBR } from "date-fns/locale";
+import { serviceDays as dates } from "./getDates";
 
-const init = new Date(2026, 2, 1);
-const end = dt.endOfMonth(init);
-
-const allOfDays = dt.eachDayOfInterval({start: init, end: end}); // get all days of month
-
-const serviceDaysUnformated = allOfDays.filter(day => {
-    const weekDay = dt.getDay(day);
-    return weekDay === 0 || weekDay === 3 || weekDay === 6;
-});
-
-const serviceDays = serviceDaysUnformated.map(date => {
-    return dt.format(date, "eeee, dd 'de' MMMM", { locale: ptBR });
+const serviceDays = dates.map(date => {
+    return `<div>${date}<input type="text" id="${date}"></div>`;
 });
 
 const fieldDate = document.querySelector("#date");
 fieldDate.innerHTML = serviceDays;
+fieldDate.innerHTML += `<div><input id="confirm" type="button" value="Confirmar escala"></div>`;
