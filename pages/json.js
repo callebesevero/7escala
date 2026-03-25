82a08eabbbcba742eb3da7e4f0fa24faee041ab6
Callebe Severo <callebesev@gmail.com>
2026-03-25 09:33:55 -0300

    split people

--- pages/json.js
+++ pages/json.js
@@ -2,13 +2,21 @@ export function parseToJSON() {
     const confirmButton = document.querySelector("#confirm");
     const dates = document.querySelector("#date");
     
-    confirmButton.addEventListener("click", (event) => {
+    confirmButton.addEventListener("click", () => {
         const label = Array.from(document.querySelectorAll("label")).map(lb => { // Array.from() because document.querySelectorAll("label") returns a NodeList
             return lb.textContent;
         });
-        const input = Array.from(document.querySelectorAll("input")).map(inp => {
-            return inp.value;
+        const input = Array.from(document.querySelectorAll("#date > div > input")).map(inp => {
+            let people = inp.value;
+            if (people.includes(",")) {
+                const splitedPeople = people.split(",");
+                return splitedPeople;
+            } else {
+                const splitedPeople = people.trim();
+                return splitedPeople;
+            };
         });
+        console.log(input);
 
         const escalaArray = label.map((lb, i) => {
             return {
@@ -16,7 +24,10 @@ export function parseToJSON() {
                 "people": input[i]
             };
         });
+        console.log(escalaArray);
 
         const escalaJSON = JSON.stringify(escalaArray);
+        console.log(escalaJSON);
+        return escalaJSON;
     });
 };
