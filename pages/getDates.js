295ee6d153d649a1b750911bfc23e6ac0a7f609d
Callebe Severo <callebesev@gmail.com>
2026-03-25 12:04:35 -0300

    simplify getDates

--- pages/getDates.js
+++ pages/getDates.js
@@ -1,18 +1,22 @@
 import * as dt from "date-fns";
 import { ptBR } from "date-fns/locale";
 
-const init = new Date(2026, 2, 1);
-const end = dt.endOfMonth(init);
-
-const allOfDays = dt.eachDayOfInterval({start: init, end: end}); // get all days of month
-
-const serviceDaysUnformated = allOfDays.filter(day => {
-    const weekDay = dt.getDay(day);
-    return weekDay === 0 || weekDay === 3 || weekDay === 6;
-});
-
-const serviceDays = serviceDaysUnformated.map(date => {
-    return dt.format(date, "eeee, dd 'de' MMMM", { locale: ptBR });
-});
-
-export default serviceDays
+export default function getServiceDays(
+    year=2026,
+    month=2
+) {
+    const init = new Date(year, month, 1);
+    const end = dt.endOfMonth(init);
+    
+    const allOfDays = dt.eachDayOfInterval({start: init, end: end}); // get all days of month
+    
+    const serviceDaysUnformated = allOfDays.filter(day => {
+        const weekDay = dt.getDay(day);
+        return weekDay === 0 || weekDay === 3 || weekDay === 6;
+    });
+    
+    const serviceDays = serviceDaysUnformated.map(date => {
+        return dt.format(date, "eeee, dd 'de' MMMM", { locale: ptBR });
+    });
+    return serviceDays;
+};
