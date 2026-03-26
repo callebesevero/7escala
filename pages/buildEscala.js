139806ca61aa36eb1e7161b0c84517d5ecbfb4c5
Callebe Severo <callebesev@gmail.com>
2026-03-26 09:57:39 -0300

    add empty space if have not domingo or quarta

--- pages/buildEscala.js
+++ pages/buildEscala.js
@@ -1,12 +1,21 @@
 export async function buildEscala() {
     const module = await import("./getDates");
-    const dates = module.default(2026, 2);
+    const dates = module.default(2026, 3);
     
     const divCalendar = document.querySelector("#calendar");
 
-    divCalendar.innerHTML += `<div class="day-name">Domingo</div><div class="day-name">Quarta</div><div class="day-name">Sábado</div>`;// add schedule name days
-    const serviceDays = dates.map(date => {
-        return `<div><label for="${date}">${date}</label><input type="text" id="${date}"></div>`; // config label and input
+    divCalendar.innerHTML += `<div class="day-name">Domingo</div><div class="day-name">Quarta</div><div class="day-name">Sábado</div>`; // add schedule name days
+    const serviceDays = dates.map((date, i) => {
+        if (i === 0) {
+            const dayName = date.slice(0, date.indexOf(","));
+            if (dayName === "quarta-feira") {
+                return `<div></div>` + `<div><label for="${date}">${date}</label><input type="text" id="${date}"></div>`; // config label and input
+            } else if (dayName === "sábado") {
+                return `<div></div><div></div>` + `<div><label for="${date}">${date}</label><input type="text" id="${date}"></div>`; // config label and input
+            };
+        } else {
+            return `<div><label for="${date}">${date}</label><input type="text" id="${date}"></div>`; // config label and input
+        };
     });
     serviceDays.forEach(field => {
         return divCalendar.innerHTML += field; // add label and input on html
