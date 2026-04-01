f6d2e5265f03c0edd9a2885aa8409944842beca5
Callebe Severo <callebesev@gmail.com>
2026-04-01 12:35:52 -0300

    require to firebase

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,30 @@
 import { parseToJSON, checkMemberRepetition } from "./json";
-import { buildEscala } from "./escala";
+import { buildEscala, buildReadyEscala } from "./escala";
 import { printEscala } from "./print";
+import { get, child, set, ref } from "firebase/database";
+import { db } from "./firebaseConfig";
 
-await buildEscala();
+const filePath = "abril de 2026";
 
-const confirmButton = document.querySelector("#confirm");
+get(child(ref(db), filePath)).then(async (snapshot) => {
+    if (snapshot.exists()) {
+        const data = JSON.parse(snapshot.val());
 
-confirmButton.addEventListener("click", async () => {
-    const escalaJSON = await parseToJSON(); // JSON schedule
-    console.log(escalaJSON);
+        buildReadyEscala(data);
+    } else {
+        await buildEscala();
+        
+        // Add escala
+        const confirmButton = document.querySelector("#confirm");
+        confirmButton.addEventListener("click", async () => {
+            const escalaJSON = await parseToJSON(); // JSON schedule
+            console.log(escalaJSON);
+        
+            set(ref(db, filePath), escalaJSON);
+        });
+    }
+}).catch((error) => {
+    console.error("Error fetching data:", error);
 });
 
 // if (!checkMemberRepetition(escalaJSON) || checkMemberRepetition(escalaJSON) && button) {
