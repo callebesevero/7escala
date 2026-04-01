ed4616f44f3514db3efd51b2f8d4f52d35dcba75
Callebe Severo <callebesev@gmail.com>
2026-04-01 09:26:30 -0300

    organizing folder

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        // Nome: caminho do arquivo
+        main: resolve(__dirname, '../../index.html'),
+        comunicacao: resolve(__dirname, 'src/pages/comunicacao.html'),
+        diaconato: resolve(__dirname, 'src/pages/diaconato.html'),
+        escola: resolve(__dirname, 'src/pages/escola-sabatina.html'),
+        musica: resolve(__dirname, 'src/pages/musica.html'),
+        pregacao: resolve(__dirname, 'src/pages/pregacao.html'),
+        sonoplastia: resolve(__dirname, 'src/pages/sonoplastia.html'),
+      },
+    },
+  },
+});
